129efec8985ed40010bd8617ae63d96b865ede78
DominikaDuralek <dominikaduralek@gmail.com>
2024-04-30 23:28:35 +0200

    added light/dark mode toggle

---
+++ src/app/data-chart/data-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-chart.component.html',
+  styleUrl: './data-chart.component.css'
+})
+export class DataChartComponent {
+
+}
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,5 +11,16 @@ import {RouterModule} from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  
+  toggleTheme() {
+    document.body.classList.toggle('light-mode');
+    if (document.body.classList.contains('light-mode')) {
+      // if current theme is light
+      document.querySelector(".sidebar__sun")?.setAttribute("style", "display: none;");
+      document.querySelector(".sidebar__moon")?.setAttribute("style", "display: inline-block;");
+    } else {
+      // if current theme is dark
+      document.querySelector(".sidebar__moon")?.setAttribute("style", "display: none;");
+      document.querySelector(".sidebar__sun")?.setAttribute("style", "display: inline-block;");
+    }
+  }
 }
