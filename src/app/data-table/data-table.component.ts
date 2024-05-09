9600c058d726dd92f9af53f199c033d2b23d620d
DominikaDuralek <dominikaduralek@gmail.com>
2024-05-09 15:23:55 +0200

    Added date picker logic, improved data-table

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -11,13 +11,15 @@ import {MatSelectModule} from '@angular/material/select';
     MatTableModule,
     MatButtonToggleGroup,
     MatButtonToggleModule,
-    MatSelectModule
+    MatSelectModule,
   ],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.css'
 })
 export class DataTableComponent{
   @ViewChild('table') table!: ElementRef;
+  @ViewChild('tableDataFrom') tableDataFrom!: ElementRef;
+  @ViewChild('tableDataTo') tableDataTo!: ElementRef;
   @ViewChild('tableSortValue') tableSortValue!: ElementRef;
   @ViewChild('tableSortButton') tableSortOrderButton!: MatButtonToggleGroup;
 
@@ -30,7 +32,10 @@ export class DataTableComponent{
   weatherDataSorted: (string | number)[][] = [];
   sortValue: string = 'Id';
   sortOrder: string = 'asc';
+  dateFrom: string = '';
+  dateTo: string = '';
 
+  // initial table data
   ngAfterViewInit() {
     this.updateTableData();
   }
@@ -39,19 +44,35 @@ export class DataTableComponent{
     this.dataSource = this.weatherDataAll;
   }
 
+  // start date changed
+  onSortDataFromChange() {
+    this.dateFrom = this.tableDataFrom.nativeElement.value;
+    this.updateTableDataSorted();
+  }
+
+  // end date changed
+  onSortDataToChange() {
+    this.dateTo = this.tableDataTo.nativeElement.value;
+    this.updateTableDataSorted();
+  }
+
+  // sort by changed
   onSortValueChange() {
     this.sortValue = this.tableSortValue.nativeElement.value;
     this.updateTableDataSorted();
   }
 
+  // asc/desc value changed
   onSortOrderChange(event: MatButtonToggleChange) {
+    console.log(this.sortOrder.valueOf);
     this.sortOrder = event.value;
     this.updateTableDataSorted();
   }
 
+  // update table data after a change
   updateTableDataSorted(){
     this.weatherDataSorted = [];
-    this.dataSource = this.weatherDataService.getSortedRecords(this.sortValue, this.sortOrder);
+    this.dataSource = this.weatherDataService.getSortedRecords(this.sortValue, this.sortOrder, this.dateFrom, this.dateTo);
   }
 
 }
