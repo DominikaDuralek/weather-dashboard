9f4cb60b4b227b7d59cebc7c3191dc87b83ebdef
DominikaDuralek <dominikaduralek@gmail.com>
2024-05-09 13:32:34 +0200

    data-table logic completed

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,37 +1,57 @@
 import { Component, ElementRef, ViewChild, AfterViewInit, inject } from '@angular/core';
 import { WeatherDataService } from '../weather-data.service'
 import {MatTableModule} from '@angular/material/table';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {MatButtonToggleChange, MatButtonToggleModule, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'data-table',
   standalone: true,
   imports: [
     MatTableModule,
+    MatButtonToggleGroup,
     MatButtonToggleModule,
-    MatButtonToggleGroup
+    MatSelectModule
   ],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.css'
 })
 export class DataTableComponent{
   @ViewChild('table') table!: ElementRef;
-  // @ViewChild('tableSortButton') tableSortButton!: MatButtonToggleModule;
-  @ViewChild('tableSortButton') tableSortButton!: MatButtonToggleGroup;
+  @ViewChild('tableSortValue') tableSortValue!: ElementRef;
+  @ViewChild('tableSortButton') tableSortOrderButton!: MatButtonToggleGroup;
 
   weatherDataService: WeatherDataService = inject(WeatherDataService);
 
-  // constructor() {
-  //   console.log(this.tableSortButton.value);
-  // }
+  weatherDataAll: (string | number)[][] = this.weatherDataService.getAllRecords();
+  dataSource = this.weatherDataAll;
+  displayedColumns = ['id', 'date', 'time', 'temperature', 'humidity', 'pressure', 'rain', 'windspeed', 'winddirection', 'light', 'pm10', 'pm25'];
+
+  weatherDataSorted: (string | number)[][] = [];
+  sortValue: string = 'Id';
+  sortOrder: string = 'asc';
 
   ngAfterViewInit() {
-    // Accessing the value of the MatButtonToggleGroup
-    console.log(this.tableSortButton.value);
+    this.updateTableData();
   }
 
-  weatherDataAll: (string | number)[][] = this.weatherDataService.getAllRecords();
-  displayedColumns = ['id', 'date', 'time', 'temperature', 'humidity', 'pressure', 'rain', 'windspeed', 'winddirection', 'light', 'pm10', 'pm25'];
-  dataSource = this.weatherDataAll;
+  updateTableData() {
+    this.dataSource = this.weatherDataAll;
+  }
+
+  onSortValueChange() {
+    this.sortValue = this.tableSortValue.nativeElement.value;
+    this.updateTableDataSorted();
+  }
+
+  onSortOrderChange(event: MatButtonToggleChange) {
+    this.sortOrder = event.value;
+    this.updateTableDataSorted();
+  }
+
+  updateTableDataSorted(){
+    this.weatherDataSorted = [];
+    this.dataSource = this.weatherDataService.getSortedRecords(this.sortValue, this.sortOrder);
+  }
+
 }
